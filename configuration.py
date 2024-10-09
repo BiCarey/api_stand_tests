c9b395cd9f7f26b028abab1927487037f68ac4aa
BIANCA TONANTZIN CHAVEZ SANCHEZ <bianca.chavez@conanp.gob.mx>
2024-10-09 16:23:57 -0500

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-283c3cec-7fac-4c3c-b9e1-ba25ea3944aa.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
